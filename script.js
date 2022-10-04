42a705d71c43d7c5cb7ede79d26851a4065335ed
sophanna <sophanna.ly88@gmail.com>
2022-10-04 22:43:30 +1100

    array filter

--- script.js
+++ script.js
@@ -96,9 +96,22 @@ createUserNames(accounts); // stw
 console.log(accounts);
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// FILTER array
+const deposits = movements.filter(mov => mov > 0);
+console.log(movements);
+console.log(deposits);
+// using for of loop
+const depositsFor = [];
+for (const mov of movements) if (mov > 0) depositsFor.push(mov);
+console.log(depositsFor);
+
+const withdrawals = movements.filter(mov => mov < 0);
+console.log(withdrawals);
+
 /*
 // MAP method
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 const eurToUsd = 1.1;
 // mothod 1: callback function
 const movementsUSD = movements.map(mov => mov * eurToUsd);
